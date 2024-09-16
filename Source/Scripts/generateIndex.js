c56d56b8a8300bb02c65b9fda8fda73ad3ab51cf
Naut van der Winden <supposedbrit@gmail.com>
2024-09-16 13:13:43 +0200

    Edits Edited new project Reversed project order (to newest first). Maybe I should add a sort function? Removed vibebot project

--- Source/Scripts/generateIndex.js
+++ Source/Scripts/generateIndex.js
@@ -5,6 +5,6 @@ function createElementFromHTML(htmlString) {
   // Change this to div.childNodes to support multiple top-level nodes.
   document.getElementById("cp").appendChild(div.firstChild);
 };
-for (const [key, value] of Object.entries(JSON.parse(data))) {
+for (const [key, value] of Object.entries(JSON.parse(data)).reverse()) {
   createElementFromHTML(`<a class="project" href="/project.html?p=${key}" onmouseenter="hover('audio${key}')"><audio id="audio${key}" src="Source/Audio/hover.mp3" preload="auto"></audio><img alt="Cover image for project: ${value["Name"]}" src="Source/Images/${value["Cover"]}"/><p>${value["Name"]}</p></a>`);
 }
