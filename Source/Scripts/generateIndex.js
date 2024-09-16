2d0e39b50c566b98e79a48422bc17b5f62936257
Naut van der Winden <supposedbrit@gmail.com>
2024-09-16 14:06:11 +0200

    Added ids for project objects

--- Source/Scripts/generateIndex.js
+++ Source/Scripts/generateIndex.js
@@ -6,5 +6,5 @@ function createElementFromHTML(htmlString) {
   document.getElementById("cp").appendChild(div.firstChild);
 };
 for (const [key, value] of Object.entries(JSON.parse(data)).reverse()) {
-  createElementFromHTML(`<a class="project" href="/project.html?p=${key}" onmouseenter="hover('audio${key}')"><audio id="audio${key}" src="Source/Audio/hover.mp3" preload="auto"></audio><img alt="Cover image for project: ${value["Name"]}" src="Source/Images/${value["Cover"]}"/><p>${value["Name"]}</p></a>`);
+  createElementFromHTML(`<a class="project" id="project_${key}" href="/project.html?p=${key}" onmouseenter="hover('audio${key}')"><audio id="audio${key}" src="Source/Audio/hover.mp3" preload="auto"></audio><img alt="Cover image for project: ${value["Name"]}" src="Source/Images/${value["Cover"]}"/><p>${value["Name"]}</p></a>`);
 }
