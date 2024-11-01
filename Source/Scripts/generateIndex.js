f64217311adb787a1880756829d0ad523d071908
Naut van der Winden <supposedbrit@gmail.com>
2024-11-01 13:13:21 +0100

    Added a sorting function

--- Source/Scripts/generateIndex.js
+++ Source/Scripts/generateIndex.js
@@ -1,10 +1,33 @@
+projects = [];
+projectsJSON = Object.entries(JSON.parse(data));
+sortList = [];
+
+sortSelec = document.getElementById("sort");
+
 function createElementFromHTML(htmlString) {
   var div = document.createElement('div');
   div.innerHTML = htmlString.trim();
 
   // Change this to div.childNodes to support multiple top-level nodes.
+  projects.push(div.firstChild);
   document.getElementById("cp").appendChild(div.firstChild);
 };
-for (const [key, value] of Object.entries(JSON.parse(data)).reverse()) {
-  createElementFromHTML(`<a class="project" id="project_${key}" href="/project.html?p=${key}" onmouseenter="hover('audio${key}')"><audio id="audio${key}" src="Source/Audio/hover.mp3" preload="auto"></audio><img alt="Cover image for project: ${value["Name"]}" src="Source/Images/${value["Cover"]}"/><p>${value["Name"]}</p></a>`);
+
+function generateIndex(){
+  projectsJSON = Object.entries(JSON.parse(data));
+  if (sortSelec.value == "old"){
+    sorted_data = projectsJSON
+  } else if (sortSelec.value == "new"){
+    sorted_data = projectsJSON.reverse()
+  }
+
+  projects.forEach(project => {
+    project.remove();
+  });
+  for (const [key, value] of sorted_data) {
+    createElementFromHTML(`<a class="project" id="project_${key}" href="/project.html?p=${key}" onmouseenter="hover('audio${key}')"><audio id="audio${key}" src="Source/Audio/hover.mp3" preload="auto"></audio><img alt="Cover image for project: ${value["Name"]}" src="Source/Images/${value["Cover"]}"/><p>${value["Name"]}</p></a>`);
+  }
+
 }
+
+generateIndex()
